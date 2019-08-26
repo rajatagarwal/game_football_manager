26b021334c847353a3162fb206849ddca0f8d799
Rajat <rrajatagarwal@gmail.com>
2019-08-26 15:21:57 +0100

    added logic template for rating adjustment task

--- helpers.py
+++ helpers.py
@@ -16,3 +16,19 @@ def select_from_menu(menu_options: dict):
 def goals_scored(rating):
 	# rating * random number between 1 to 5 / rating
 	return int(( rating * random.randint(1,5) / rating ))
+
+
+# def adjust_ratings(rating, result):
+# 	"""
+# 	Logic to adjust rating based on the result
+
+# 	"""
+# 	if away.won:
+# 		if away.rating > home.rating:
+# 			diff = away.rating - home.rating
+# 			# send positive number, which need to be added to the team
+# 			return diff/22
+# 		else:
+# 			# high rating team lost to low rating team. more reward and penalty
+# 			return diff/11 from higher team
+# 			and may be diff/22 for lower team
